8ad9b6bbcc63304b1d5bfa8aaab9f0ac9c4f2822
pabcos <pablo.vilaboa@ctag.com>
2019-08-30 14:04:34 +0200

    vars, tipos, tablas, fks, mtm,  echo  :)
    No me costo nada , y eso me  jode

--- Factorio 3 La resurreccion/src/bd/Leer.java
+++ Factorio 3 La resurreccion/src/bd/Leer.java
@@ -9,108 +9,161 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class Leer {
-  
-  ArrayList<String>tablas;
+  Map<Integer, Boolean> mtm;
+  ArrayList<String> tablas;
   Map<Integer, ArrayList<String>> fks;
   DatabaseMetaData databaseMetaData;
-  public Leer(ArrayList<String>tablas,Map<Integer, ArrayList<String>> fks ) {
-    this.tablas=tablas;
-    this.fks=fks; 
-    
-    
+  Map<Integer, ArrayList<String>> vars;
+  Map<Integer, ArrayList<String>> types;
+
+  // CONSTRUCTOR
+  public Leer(ArrayList<String> tablas, Map<Integer, ArrayList<String>> fks,
+      Map<Integer, Boolean> mtm, Map<Integer, ArrayList<String>> vars,
+      Map<Integer, ArrayList<String>> types) {
+    this.tablas = tablas;
+    this.fks = fks;
+    this.mtm = mtm;
+    this.vars = vars;
+    this.types = types;
+
   }
 
-  public  void start()throws SQLException {
+  public void start() throws SQLException {
 
-    Connection con=connect();
+    Connection con = connect();
     databaseMetaData = con.getMetaData();
     getTableNames();
+    getMTM();
     getFKs();
-    
-    
-  }
-  
-  
-  
-  public  void getFKs( ) throws SQLException {
-    
-    int c=-1;
-    for (String actualTable : tablas) {
-      c++;
-  ResultSet fkLoop = databaseMetaData.getImportedKeys(null, null, actualTable);
-  ArrayList<String>var=new ArrayList<>();
-  System.out.println("\n-FOREIGN KEYS   "+actualTable);
-  while(fkLoop.next())
-  {
-    var.add( fkLoop.getString("FKCOLUMN_NAME"));
-    System.out.println(fkLoop.getString("PKTABLE_NAME") + "---" + fkLoop.getString("PKCOLUMN_NAME") + "===" + fkLoop.getString("FKTABLE_NAME") + "---" + fkLoop.getString("FKCOLUMN_NAME"));
+    getFields();
 
   }
-  if(!var.isEmpty()){fks.put(c, var);}
-    }    
-    
+
+  public void getFields() throws SQLException {
+
+    int c = -1;
+    for (String actualTable : tablas) {
+      c++;
+      System.err.println("\n\n");
+      ArrayList<String> dataVars = new ArrayList<>();
+      ArrayList<String> dataTypes = new ArrayList<>();
+      ResultSet columns = databaseMetaData.getColumns(null, null, actualTable, null);
+      while (columns.next()) {
+        String columnName = columns.getString("COLUMN_NAME");
+        String datatype = columns.getString("TYPE_NAME");
+
+        if (!mtm.get(c)
+            && (fks.get(c) == null || (fks.get(c) != null && !fks.get(c).contains(columnName)))) {
+          dataVars.add(columnName);
+          dataTypes.add(datatype);
+          
+          System.err.println(columnName + "  " + dataTypes.size() + "   ");
+
+        }
+      }
+System.err.println(vars.size());
+      vars.put(c, dataVars);
+      types.put(c, dataTypes);
+    }
   }
-  
-  public  Connection connect() throws SQLException {
-    
-    Connection con=null;
-    String url="jdbc:mysql://localhost:3306/totem";
-    String user="root",password="";
-    
+
+  // Por ahora si tiene más de un Pk La voy a poner aquí
+  public void getMTM() throws SQLException {
+
+    int c = -1;
+    for (String actualTable : tablas) {
+      c++;
+      ResultSet pkFind = databaseMetaData.getPrimaryKeys(null, null, actualTable);
+      pkFind.last();
+      if (pkFind.getRow() > 1) {
+        // TODO MTM
+        mtm.put(c, true);
+
+      } else {
+        mtm.put(c, false);
+      }
+    }
+  }
+
+  public void getFKs() throws SQLException {
+
+    int c = -1;
+    for (String actualTable : tablas) {
+      c++;
+      if (!mtm.get(c)) {
+        ResultSet fkFind = databaseMetaData.getImportedKeys(null, null, actualTable);
+        ArrayList<String> var = new ArrayList<>();
+        while (fkFind.next()) {
+          var.add(fkFind.getString("FKCOLUMN_NAME"));
+        }
+        if (!var.isEmpty()) {
+          fks.put(c, var);
+        }
+      }
+    }
+  }
+
+  public Connection connect() throws SQLException {
+
+    Connection con = null;
+    String url = "jdbc:mysql://localhost:3306/totem";
+    String user = "root";
+    String password = "";
+
     try {
       Class.forName("com.mysql.jdbc.Driver");
-     con= DriverManager.getConnection(url, user,password);
+      con = DriverManager.getConnection(url, user, password);
     } catch (ClassNotFoundException e) {
     }
     return con;
   }
 
-    public  void getTableNames() throws SQLException {
-    
-    //TABLE NAMES
-      ResultSet tables = databaseMetaData.getTables(null, null, null, new String[] { "TABLE" });
-    
-    while (tables.next()) {
-      String actualTable=tables.getString("TABLE_NAME");
-      if(!actualTable.equalsIgnoreCase("SEQUENCE")) {
-      tablas.add(actualTable); }
-      
-    }
-    }
-}
-//    //GetPrimarykeys
-//      ResultSet PK = databaseMetaData.getPrimaryKeys(null,null, actualTable);
-//      System.out.println("------------PRIMARY KEYS-------------");
-//      while(PK.next())
-//      {
-//          System.out.println(PK.getString("COLUMN_NAME") + "===" + PK.getString("PK_NAME"));
-//      }
-//      System.out.println("---------------------------------------------");
-//      
-//      
-//    //Get Foreign Keys
-//      ResultSet FK = databaseMetaData.getImportedKeys(null, null, actualTable);
-//      System.out.println("------------FOREIGN KEYS-------------");
-//      while(FK.next())
-//      {
-//          System.out.println(FK.getString("PKTABLE_NAME") + "---" + FK.getString("PKCOLUMN_NAME") + "===" + FK.getString("FKTABLE_NAME") + "---" + FK.getString("FKCOLUMN_NAME"));
-//      }
-//      System.out.println("---------------------------------------------");
-//    //TABLE COLUMNS
-//      ResultSet columns = databaseMetaData.getColumns(null,null, tables.getString("TABLE_NAME"), null);
-//      while(columns.next())
-//      {
-//          String columnName = columns.getString("COLUMN_NAME");
-//          String datatype = columns.getString("TYPE_NAME");
-//        System.out.println(columnName + "-type " + datatype);
-//          
-//          String columnsize = columns.getString("COLUMN_SIZE");
-////          String decimaldigits = columns.getString("DECIMAL_DIGITS");
-//          String isNullable = columns.getString("IS_NULLABLE");
-//          String is_autoIncrment = columns.getString("IS_AUTOINCREMENT");
-//          //Printing results
-//          System.out.println(columnName + "-type " + datatype + "-col " + columnsize +  "-null " + isNullable + "-auto " + is_autoIncrment);
-//      }
-//    }
-    
+  public void getTableNames() throws SQLException {
 
+    // TABLE NAMES
+    ResultSet tables = databaseMetaData.getTables(null, null, null, new String[] { "TABLE" });
+
+    while (tables.next()) {
+      String actualTable = tables.getString("TABLE_NAME");
+      if (!actualTable.equalsIgnoreCase("SEQUENCE")) {
+        tablas.add(actualTable);
+      }
+    }
+  }
+}
+// //GetPrimarykeys
+// ResultSet PK = databaseMetaData.getPrimaryKeys(null,null, actualTable);
+// System.out.println("------------PRIMARY KEYS-------------");
+// while(PK.next())
+// {
+// System.out.println(PK.getString("COLUMN_NAME") + "===" + PK.getString("PK_NAME"));
+// }
+// System.out.println("---------------------------------------------");
+//
+//
+// //Get Foreign Keys
+// ResultSet FK = databaseMetaData.getImportedKeys(null, null, actualTable);
+// System.out.println("------------FOREIGN KEYS-------------");
+// while(FK.next())
+// {
+// System.out.println(FK.getString("PKTABLE_NAME") + "---" + FK.getString("PKCOLUMN_NAME") + "===" +
+// FK.getString("FKTABLE_NAME") + "---" + FK.getString("FKCOLUMN_NAME"));
+// }
+// System.out.println("---------------------------------------------");
+// //TABLE COLUMNS
+// ResultSet columns = databaseMetaData.getColumns(null,null, tables.getString("TABLE_NAME"), null);
+// while(columns.next())
+// {
+// String columnName = columns.getString("COLUMN_NAME");
+// String datatype = columns.getString("TYPE_NAME");
+// System.out.println(columnName + "-type " + datatype);
+//
+// String columnsize = columns.getString("COLUMN_SIZE");
+//// String decimaldigits = columns.getString("DECIMAL_DIGITS");
+// String isNullable = columns.getString("IS_NULLABLE");
+// String is_autoIncrment = columns.getString("IS_AUTOINCREMENT");
+// //Printing results
+// System.out.println(columnName + "-type " + datatype + "-col " + columnsize + "-null " +
+// isNullable + "-auto " + is_autoIncrment);
+// }
+// }
--- Factorio 3 La resurreccion/src/main/Main.java
+++ Factorio 3 La resurreccion/src/main/Main.java
@@ -13,118 +13,122 @@ import bd.Leer;
 public class Main {
 
   public static void main(String[] args) throws IOException, SQLException {
-    //tipo 3 =MANY TO MANY CON PARAMETRO
-    Map<Integer,Integer>tipo=new HashMap<>();
+    // tipo 3 =MANY TO MANY CON PARAMETRO
+    Map<Integer, Integer> tipo = new HashMap<>();
     Map<Integer, String[]> Mtm = new HashMap<>();
-    Map<Integer, ArrayList<Integer>> mto  = new HashMap<>();
+    Map<Integer, ArrayList<Integer>> mto = new HashMap<>();
     Map<Integer, ArrayList<Integer>> otm = new HashMap<>();
-    Map<Integer, Boolean> mtm = new HashMap<>();//SI LA TABLA ES MTM O NO
-    Map<Integer, ArrayList<String>> fks = new HashMap<>();//NOMBRE DE TODAS LAS FKS
-    Map<Integer, ArrayList<String>> vars = new HashMap<>();//nombre de las variables de los fields
-    Map<Integer, ArrayList<String>> tipos = new HashMap<>();//tipo de las variables de los fields
-  Map<Integer, ArrayList<String>> fields = new HashMap<>();//fields sin limpiar
-  Map<Integer, ArrayList<String>> foreign = new HashMap<>();//foreign regerences sin limpiar
-  ArrayList<String>arrayprimary=new ArrayList<>();//TODOS LOS IDS LOS QUE SON 2 SEPARADOS POR UN ESPACIO
-  ArrayList<String>tablas=new ArrayList<>();//NOMBRE D ELAS TABLAS
-    Bdd.bdd(fields,foreign,arrayprimary,tablas,mtm,tipo);
+    Map<Integer, Boolean> mtm = new HashMap<>();// SI LA TABLA ES MTM O NO
+    Map<Integer, ArrayList<String>> fks = new HashMap<>();// NOMBRE DE TODAS LAS FKS
+    Map<Integer, ArrayList<String>> vars = new HashMap<>();// nombre de las variables de los fields
+    Map<Integer, ArrayList<String>> tipos = new HashMap<>();// tipo de las variables de los fields
+    Map<Integer, ArrayList<String>> fields = new HashMap<>();// fields sin limpiar
+    Map<Integer, ArrayList<String>> foreign = new HashMap<>();// foreign regerences sin limpiar
+    ArrayList<String> arrayprimary = new ArrayList<>();// TODOS LOS IDS LOS QUE SON 2 SEPARADOS POR
+                                                       // UN ESPACIO
+    ArrayList<String> tablas = new ArrayList<>();// NOMBRE D ELAS TABLAS
+    Bdd.bdd(fields, foreign, arrayprimary, tablas, mtm, tipo);
     System.err.println("sdad");
-    ConverToPojo.pojo(vars,tipos,fields, foreign, arrayprimary,tablas,fks,mtm,otm,mto,Mtm);
-    Scanner s=new Scanner(System.in);
-    
-    
+    ConverToPojo.pojo(vars, tipos, fields, foreign, arrayprimary, tablas, fks, mtm, otm, mto, Mtm);
+    Scanner s = new Scanner(System.in);
 
-    
-/*   int cd=0;
-    while(1>cd) {
-      System.out.println("escrbe" );
-      String a=s.nextLine();
-      metodos.mayusq(a);
-      
-    }*/
-    
-    
+    /*
+     * int cd=0; while(1>cd) { System.out.println("escrbe" ); String a=s.nextLine();
+     * metodos.mayusq(a);
+     * 
+     * }
+     */
 
-    
-    System.out.println("\n\n\nMAIN" );
-    int c=0;
-//    for(String i:arrayprimary) {
-//      c++;
-//      System.out.println(i+"    "+c ); 
-//      
-//    }
-    
-    
-    
-    
-      for (int i = 1; i < tablas.size() + 1; i++) {
-        //poner tipo=3 para base entities
-        if(tipo.get(i)==1) {
-          tipo.put(i, 3);
-          System.out.println( "\n"+tablas.get(i-1)+"            "+"   cosas  "+arrayprimary.get(i-1)+""); 
-         String word;
-         System.out.println("vars antes   "+vars.get(i) ); 
-         c=0;
-          do {
-            word=null;
-            word=metodos.word(arrayprimary.get(i-1), c+1);
-                        if(word!=null) {
-                          for (int j = 0; j < vars.get(i).size(); j++) {
-                            word=metodos.despital(word);
-                            System.out.println("here   "+vars.get(i).get(j)+"   "+word ); 
-                            if(vars.get(i).get(j).equalsIgnoreCase(word)){
-                              vars.get(i).remove(j);
-                              tipos.get(i).remove(j);}
-                          }        }             
-                        c++;
-              }while(word!=null);
-          System.out.println("vars despues   "+vars.get(i) ); 
-        }
-        
+    System.out.println("\n\n\nMAIN");
+    int c = 0;
+    // for(String i:arrayprimary) {
+    // c++;
+    // System.out.println(i+" "+c );
+    //
+    // }
+
+    for (int i = 1; i < tablas.size() + 1; i++) {
+      // poner tipo=3 para base entities
+      if (tipo.get(i) == 1) {
+        tipo.put(i, 3);
+        System.out.println("\n" + tablas.get(i - 1) + "            " + "   cosas  "
+            + arrayprimary.get(i - 1) + "");
+        String word;
+        System.out.println("vars antes   " + vars.get(i));
+        c = 0;
+        do {
+          word = null;
+          word = metodos.word(arrayprimary.get(i - 1), c + 1);
+          if (word != null) {
+            for (int j = 0; j < vars.get(i).size(); j++) {
+              word = metodos.despital(word);
+              System.out.println("here   " + vars.get(i).get(j) + "   " + word);
+              if (vars.get(i).get(j).equalsIgnoreCase(word)) {
+                vars.get(i).remove(j);
+                tipos.get(i).remove(j);
+              }
+            }
+          }
+          c++;
+        } while (word != null);
+        System.out.println("vars despues   " + vars.get(i));
       }
-      
-      //Si  hay mas de una pk perooooo no es mtm
-      for(int i=1;i<tablas.size();i++){  
-        if(mtm.get(i)==true) {
-          System.out.println(tablas.get(i-1) ); 
-          if(Mtm.get(i)==null) { tipo.put(i, 4); }
+
+    }
+
+    // Si hay mas de una pk perooooo no es mtm
+    for (int i = 1; i < tablas.size(); i++) {
+      if (mtm.get(i) == true) {
+        System.out.println(tablas.get(i - 1));
+        if (Mtm.get(i) == null) {
+          tipo.put(i, 4);
         }
-        
       }
-          
-      Map<Integer, ArrayList<String>> fksTest = new HashMap<>();//NOMBRE DE TODAS LAS FKS
-      ArrayList<String>tablasTest=new ArrayList<>();//NOMBRE D ELAS TABLAS
-      Leer leer=new Leer(tablasTest,fksTest);
-      leer.start();
-      System.err.println(tablasTest.size());
-      System.exit(1);
-    AgregadoPLUSPLUS.test(vars,tipos,tablas,fks,mtm,otm,mto,Mtm,tipo);
-    
-//                                                      TABLAS
-//    [Line, TOT_Page, TOT_Button, TOT_DocType, TOT_Document, TOT_ButtonDoc, TOT_Style, TOT_ButtonStyle, TOT_Configuration]
-      
-//                                                      VARS
-//     {1=[idLine, name, active], 2=[idPage, name, order, active], 3=[idButton, name, shortName, x, y, w, h, active, order], 
-//    4=[idDocType, name], 5=[idDoc, name, link], 6=[], 7=[idStyle, name, styleClass], 8=[idButton, idStyle], 9=[idConfig, value]}
-    
-//                                                      TIPOS
-//{1=[Integer, String, boolean], 2=[Integer, String, Integer, boolean], 3=[Integer, String, String, Integer, Integer, Integer, Integer, boolean, Integer], 
-//    4=[Integer, String], 5=[Integer, String, String], 6=[], 7=[Integer, String, String], 8=[Integer, Integer], 9=[Integer, String]}
-    
-    
-//                                                      FKS
-//    {2=[FK_Line], 3=[FK_Page], 5=[FK_DocType]}
-    
-    
-//                                                    TIPO
-//    {1=0, 2=0, 3=0, 4=0, 5=0, 6=0, 7=0, 8=0, 9=0}
-    
-//                                                     OTM
-//   {1=[2], 2=[1, 3], 4=[5]}
-    
-//                                                    MTO 
-//    {2=[1, 3], 3=[2], 5=[4]}
-    
-//                                                      MTM
-//    {1=false, 2=false, 3=false, 4=false, 5=false, 6=true, 7=false, 8=true, 9=false}
+
+    }
+
+    Map<Integer, ArrayList<String>> varsTest = new HashMap<>();// nombre de las variables de los
+                                                               // fields
+    Map<Integer, ArrayList<String>> tiposTest = new HashMap<>();// tipo de las variables de los
+                                                                // fields
+    Map<Integer, Boolean> mtmTest = new HashMap<>();// SI LA TABLA ES MTM O NO
+    Map<Integer, ArrayList<String>> fksTest = new HashMap<>();// NOMBRE DE TODAS LAS FKS
+    ArrayList<String> tablasTest = new ArrayList<>();// NOMBRE D ELAS TABLAS
+    Leer leer = new Leer(tablasTest, fksTest, mtmTest, varsTest, tiposTest);
+    leer.start();
+    System.err.println(tablasTest.size());
+    System.exit(1);
+    AgregadoPLUSPLUS.test(vars, tipos, tablas, fks, mtm, otm, mto, Mtm, tipo);
+
+    // TABLAS
+    // [Line, TOT_Page, TOT_Button, TOT_DocType, TOT_Document, TOT_ButtonDoc, TOT_Style,
+    // TOT_ButtonStyle, TOT_Configuration]
+
+    // VARS
+    // {1=[idLine, name, active], 2=[idPage, name, order, active], 3=[idButton, name, shortName, x,
+    // y, w, h, active, order],
+    // 4=[idDocType, name], 5=[idDoc, name, link], 6=[], 7=[idStyle, name, styleClass], 8=[idButton,
+    // idStyle], 9=[idConfig, value]}
+
+    // TIPOS
+    // {1=[Integer, String, boolean], 2=[Integer, String, Integer, boolean], 3=[Integer, String,
+    // String, Integer, Integer, Integer, Integer, boolean, Integer],
+    // 4=[Integer, String], 5=[Integer, String, String], 6=[], 7=[Integer, String, String],
+    // 8=[Integer, Integer], 9=[Integer, String]}
+
+    // FKS
+    // {2=[FK_Line], 3=[FK_Page], 5=[FK_DocType]}
+
+    // TIPO
+    // {1=0, 2=0, 3=0, 4=0, 5=0, 6=0, 7=0, 8=0, 9=0}
+
+    // OTM
+    // {1=[2], 2=[1, 3], 4=[5]}
+
+    // MTO
+    // {2=[1, 3], 3=[2], 5=[4]}
+
+    // MTM
+    // {1=false, 2=false, 3=false, 4=false, 5=false, 6=true, 7=false, 8=true, 9=false}
   }
 }
